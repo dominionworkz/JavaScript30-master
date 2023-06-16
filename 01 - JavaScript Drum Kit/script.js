da78b40fdfe9d7f7a90e05324d941ea6dd121302
Quincy Bulla <37887069+dominionworkz@users.noreply.github.com>
2023-06-16 19:27:53 -0400

    revised js file

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("keydown", function (e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stops audio from running
+    audio.currentTime = 0; //rewinds to the start
+    audio.play();
+    key.classList.add("playing");
+    console.log(key);
+});
+
+function removeTransition(e) {
+    if(e.propertyName !== "transform")return; //skip if not transform
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
